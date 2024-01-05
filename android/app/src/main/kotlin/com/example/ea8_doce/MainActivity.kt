62fdb9f9011732d6df4b7c1d9c9468af51b6cefd
Kelpura <weaklingsminions@gmail.com>
2024-01-05 17:49:27 +0800

    First Commit

---
+++ android/app/src/main/kotlin/com/example/ea8_doce/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ea8_doce
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,43 @@
+import 'package:ea8_doce/screens/location_screen.dart';
+
+import 'package:flutter/material.dart';
+
+import 'package:ea8_doce/services/networking.dart';
+
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class LoadingScreen extends StatefulWidget {
+  const LoadingScreen({super.key});
+
+  @override
+  State<LoadingScreen> createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    super.initState();
+    getWeatherData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+          child: SpinKitWaveSpinner(
+        size: 100.0,
+        color: Colors.greenAccent,
+      )),
+    );
+  }
+
+
+  void getWeatherData() async {
+    Networking networking = new Networking();
+    String data = await networking.getData();
+
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LocationScreen(data);
+    }));
+  }
+}
